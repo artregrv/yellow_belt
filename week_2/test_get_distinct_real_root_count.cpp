a2b5b31b6534fba11ace990f36946ecf0e1000cb
artregrv <egorov.aa@micran.ru>
2023-05-22 19:25:32 +0700

    test person added

--- week_2/test_get_distinct_real_root_count.cpp
+++ week_2/test_get_distinct_real_root_count.cpp
@@ -1,38 +1,82 @@
-#include "utest.h"
+#include <exception>
+#include <iostream>
+#include <sstream>
 
-#include <vector>
-
-int sum(int a, int b)
+template<class Lhs, class Rhs>
+void assertEqual(const Lhs &lhs, const Rhs &rhs, const std::string &hint)
 {
-	return a + b + 1;
+	if (lhs != rhs) {
+		std::ostringstream stream;
+		stream << "Assertion failed: " << lhs << " != " << rhs << " hint: " << hint;
+		throw std::runtime_error(stream.str());
+	}
 }
 
-std::vector<int> sqrIntVector(const std::vector<int> &v)
+void asset(bool value, const std::string &hint)
 {
-	std::vector<int> result;
-	for (const auto &item : v) {
-		result.push_back(item * item);
+	assertEqual(value, true, hint);
+}
+
+class UTest
+{
+public:
+	template<class TestFunction>
+	void runTest(TestFunction function, const std::string &functionName)
+	{
+		try {
+			function();
+			std::cerr << functionName << " OK\n"; 
+		} catch (std::runtime_error &error) {
+			++_failedTestsCount;
+			std::cerr << error.what() << '\n';
+		}
 	}
 
-	return result;
-} 
+	~UTest()
+	{
+		if (_failedTestsCount > 0) {
+			std::cerr << _failedTestsCount << " tests failed. Terminate.\n";
+			exit(1);
+		}
+	}
 
-void testSum()
+private:
+	size_t _failedTestsCount { 0 };
+};
+
+int GetDistinctRealRootCount(double a, double b, double c) 
 {
-	assertEqual(sum(4, 5), 9, "simple sum");
+	if (a == 0) {
+		if (b == 0) {
+			return 0;
+		}
+		return 1;
+	} 
+	
+	int d = b * b - 4 * a * c;
+
+	if (d == 0) {
+		return 1;
+	} else if (d > 0) {
+		return 2;
+	} 
+	
+	return 0;
 }
 
-void testSqrIntVector()
+void testGetDistinctRealRootCount()
 {
-	std::vector<int> expected { 1, 4, 8, 16 };
-	assertEqual(sqrIntVector({ 1, 2, 3, 4 }), expected, "simple vector");
+	assertEqual(GetDistinctRealRootCount(0, 0, 1), 0, "a = 0, b = 0, c = 1");
+	assertEqual(GetDistinctRealRootCount(0, 1, 1), 1, "a = 0, b = 1, c = 1");
+	assertEqual(GetDistinctRealRootCount(1, 1, 1), 0, "a = 1, b = 1, c = 1");
+	assertEqual(GetDistinctRealRootCount(1, 3, 1), 2, "a = 1, b = 3, c = 1");
+	assertEqual(GetDistinctRealRootCount(1, 2, 1), 1, "a = 1, b = 2, c = 1");
 }
 
 void testAll()
 {
 	UTest ut;
-	ut.runTest(testSum, "testSum");
-	ut.runTest(testSqrIntVector, "sqrIntVector");
+	ut.runTest(testGetDistinctRealRootCount, "testGetDistinctRealRootCount");
 }
 
 int main()
